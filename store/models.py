20685fc3492dd7a2125e03deda17f76cce161ba6
Elmorsi5 <mohamedelmorse5@gmail.com>
2024-10-06 04:27:22 +0300

    feat: :art: likeditems

--- store/models.py
+++ store/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product',on_delete=models.SET_NULL,related_name="+")
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
 class Product(models.Model):
     title = models.TextField(max_length=255)
     description = models.CharField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField()
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotion = models.ManyToManyField(Promotion)
 
 
-class Customer(models.Modle):
+class Customer(models.Model):
     class MembershipChoices(models.TextChoices):
         BRONZE = "B", "Bronze"
         SILVER = "S", "Silver"
@@ -35,8 +47,27 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=255, choices=PaymentStatus.choices, default=PaymentStatus.PENDING
     )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_pirce = models.DecimalField(max_digits=6, decimal_places=2)
+
 
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
